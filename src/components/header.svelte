67673c8fbb90c8d4327c83a7981fd303d350555c
zerais <davesiarez18@gmail.com>
2023-11-25 22:09:06 +0800

    initial commit + layout done

---
+++ src/components/header.svelte
@@ -0,0 +1,7 @@
+<header class="h-[20vh] w-[100%] flex justify-center items-center">
+    
+
+    <h1 class=" text-center font-arcade-n text-2xl">CAS ARCADE</h1>
+
+  
+</header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<main>
+
+  
+
+    <div class="h-[80vh] w-[100%] flex justify-evenly items-center gap-10 font-arcade-n">
+        <div class="flex justify-center items-center flex-col gap-10">
+            <a href="/game1"><div class="h-[20vh] w-[50vh] flex justify-center items-center rounded-md  border-solid border-4 border-black hover:bg-sky-200"><h2>Game 1</h2></div></a>
+            <a href="/game3"><div class="h-[20vh] w-[50vh] flex justify-center items-center rounded-md  border-solid border-4 border-black hover:bg-sky-200" ><h2>Game 3</h2></div></a>
+            <a href="/game5"><div class="h-[20vh] w-[50vh] flex justify-center items-center rounded-md  border-solid border-4  border-black hover:bg-sky-200"><h2>Game 5</h2></div></a>   
+        </div>
+        
+        <div class="flex justify-center items-center flex-col gap-10">
+            <a href="/game2"><div class="h-[20vh] w-[50vh] flex justify-center items-center rounded-md  border-solid border-4 border-black hover:bg-sky-200 "><h2>Game 2</h2></div></a>
+            <a href="/game4"><div class="h-[20vh] w-[50vh] flex justify-center items-center rounded-md  border-solid border-4 border-black hover:bg-sky-200"><h2>Game 4</h2></div></a>
+            <a href="/game6"><div class="h-[20vh] w-[50vh] flex justify-center items-center rounded-md  border-solid border-4  border-black hover:bg-sky-200"><h2>Game 6</h2></div></a>
+        </div>
+  
+    </div>
+</main>
